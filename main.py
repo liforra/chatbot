2e674eb4227de256a33ff002e40c660b3203708f
liforra <git@liforra.de>
2026-01-06 13:00:34 +0100

    feat: fully functional AI Chat bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from groq import Groq
-from lutils import config, log
+from lutils import config, log, set_log_level
+set_log_level("warn")
 
 if config["keys.groq"]:
     client = Groq(api_key=config["keys.groq"])
@@ -11,16 +12,27 @@ else:
     groq_key: bool = False
 
 
-def ask_ai(question: str) -> None:
+def append_message(msg_list: list, message: str, role: str) -> list:
+    log("debug", f"msg_list: {msg_list}")
+    log("debug", f"message: {message}")
+    log("debug", f"role: {role}")
+    
+    if role not in (("user", "assistant", "system")):
+        log("fatal", "Role Must be Assistant, User or System.")
+    message_dict = {
+        "role": role,
+        "content": message,
+    }
+    msg_list = msg_list + [message_dict]
+    if not msg_list:
+        raise Exception("Messages is now empty")
+    return msg_list
+
+
+def ask_ai(messages: list) -> list:
     completion = client.chat.completions.create(
         model="openai/gpt-oss-120b",
-        messages=[
-            {
-                "role": "system",
-                "content": f"{system_prompt}",
-            },
-            {"role": "user", "content": f"{question}"},
-        ],
+        messages= messages,
         temperature=1,
         max_completion_tokens=8192,
         top_p=1,
@@ -28,24 +40,47 @@ def ask_ai(question: str) -> None:
         stream=True,
         stop=None,
     )
-
+    full_response = ""
     for chunk in completion:
-        print(chunk.choices[0].delta.content or "", end="")
+        delta = chunk.choices[0].delta.content
+        print(delta or "", end="")
+        full_response = full_response
+        if delta:
+            full_response += delta
+    print("")
+    return full_response
 
 
-def main() -> None:
-    print("Wie kann ich dir weiterhelfen?")
+def mainloop() -> None:
+    global messages
     userinput = input("> ")
     match userinput:
         case _:
             if groq_key:
-                ask_ai(userinput)
+                messages = append_message(messages, userinput, "user")
+                response = ask_ai(messages)
+                messages = append_message(messages, response, "assistant")
+                
             else:
                 log("warn", "AI was attempted to be triggered but no API Key was set.")
                 print(
                     "Leider hab ich dich nicht ganz verstanden, bitte versuche es nochmal."
                 )
 
+messages: list
+def main() -> None:
+    global messages
+    messages = [
+            {
+                "role": "system",
+                "content": f"{system_prompt}",
+            },
+        ]
+    print("Wie kann ich dir weiterhelfen?")
+    while True:
+        mainloop()
+    
 
-while __name__ == "__main__":
+
+if __name__ == "__main__":
     main()
